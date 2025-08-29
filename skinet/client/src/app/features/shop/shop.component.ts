9047fbedbd556466f23689ce7c827b4a33007175
Marko Jovanovic <Marokos9991@outlook.com>
2025-08-29 19:55:13 +0200

    Getting the types and brands lists

--- skinet/client/src/app/features/shop/shop.component.ts
+++ skinet/client/src/app/features/shop/shop.component.ts
@@ -18,9 +18,15 @@ export class ShopComponent implements OnInit {
   products: Product[] = [];
 
 ngOnInit(): void {
-    this.shopService.getProducts().subscribe({
+    this.initializeShop();
+    };
+
+    initializeShop(){
+      this.shopService.getBrands();
+      this.shopService.getTypes();
+      this.shopService.getProducts().subscribe({
       next: response => this.products = response.data,
       error: error => console.log(error)
-    });
+    })
 }
 }
