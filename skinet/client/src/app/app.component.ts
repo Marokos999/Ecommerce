afde1dd9bd7a05113971ffe742cf15862ea7ebae
Marko Jovanovic <Marokos9991@outlook.com>
2025-08-29 19:16:31 +0200

    Making and using service component in app component

--- skinet/client/src/app/app.component.ts
+++ skinet/client/src/app/app.component.ts
@@ -1,27 +1,23 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
-import { HttpClient } from '@angular/common/http';
 import { Product } from './shared/models/products';
-import { Pagination } from './shared/models/pagination';
+import { ShopService } from './core/services/shop.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [ HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  baseUrl = signal('https://localhost:5001/api/');
-  private http = inject(HttpClient);
+  private shopService = inject(ShopService);
   protected  title = 'skinet';
   products: Product[] = [];
 
 ngOnInit(): void {
-    this.http.get<Pagination<Product>>(this.baseUrl() + 'products').subscribe({
+    this.shopService.getProducts().subscribe({
       next: response => this.products = response.data,
-      error: error => console.log(error),
-      complete: () => console.log('Request completed')
+      error: error => console.log(error)
     });
 }
 }
