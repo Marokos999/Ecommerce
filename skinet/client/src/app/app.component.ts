a2bc8cd452a0589aa64edb78983826cf52b5daf5
Marko Jovanovic <Marokos9991@outlook.com>
2025-08-29 19:27:52 +0200

    Designing the shop page

--- skinet/client/src/app/app.component.ts
+++ skinet/client/src/app/app.component.ts
@@ -1,23 +1,13 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { HeaderComponent } from './layout/header/header.component';
-import { Product } from './shared/models/products';
-import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from "./fearures/shop/shop.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ HeaderComponent],
+  imports: [HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  private shopService = inject(ShopService);
-  protected  title = 'skinet';
-  products: Product[] = [];
-
-ngOnInit(): void {
-    this.shopService.getProducts().subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error)
-    });
-}
+export class AppComponent {
+title = 'skinet';
 }
