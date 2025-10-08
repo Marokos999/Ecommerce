7e291c4ce83e62b249947a1cbf94cd9631df44c0
Marko Jovanovic <Marokos9991@outlook.com>
2025-10-08 18:32:39 +0200

    Adding toast (snackbar) notifications

---
+++ skinet/client/src/app/core/interceptors/error-interceptor.ts
@@ -0,0 +1,28 @@
+import { HttpErrorResponse, HttpEvent ,HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const route = inject(Router);
+
+  return next(req).pipe(
+    catchError((err: HttpErrorResponse) => {
+      if(err.status === 400) {
+        alert(err.error.title || err.error);
+      }
+      if(err.status === 401) {
+        alert(err.error.title || err.error);
+      }
+      if(err.status === 404) {
+        route.navigateByUrl('/not-found');
+      }
+      if(err.status === 500) {
+        const navigationExtras = {state: {error: err.error}};
+        route.navigateByUrl('/server-error', navigationExtras);
+      }
+      
+      return throwError(() => err);
+    })
+  )
+};
