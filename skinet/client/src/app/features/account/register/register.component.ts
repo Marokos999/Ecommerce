6a915f94221bddeea1018b0914e0a5fe5c41cc57
Marko Jovanovic <Marokos9991@outlook.com>
2025-10-19 22:55:31 +0200

    Creating a re-usable text input

--- skinet/client/src/app/features/account/register/register.component.ts
+++ skinet/client/src/app/features/account/register/register.component.ts
@@ -8,6 +8,7 @@ import { AccountService } from '../../../core/services/account.service';
 import { Router } from '@angular/router';
 import { SnackbarService } from '../../../core/services/snackbar.service';
 import { JsonPipe } from '@angular/common';
+import { TextInputComponent } from "../../../shared/comonents/text-input/text-input.component";
 
 @Component({
   selector: 'app-register',
@@ -20,7 +21,8 @@ import { JsonPipe } from '@angular/common';
     MatButton,
     JsonPipe,
     MatError,
-  ],
+    TextInputComponent
+],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
